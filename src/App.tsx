a62d064d96a2b7113b6db78acb7412538df3373d
Kenzo Sebastian <kenzosebastian4@gmail.com>
2026-03-18 20:44:24 +0700

    setup: shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <h1 className="text-6xl font-bold underline">React + TypeScript</h1>
+      <Button className="mt-4">Click Me</Button>
     </>
   );
 }
